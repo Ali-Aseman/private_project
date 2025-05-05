9f882dc69458cc1336d3ab27931cf8fda274ed2a
Ali <96992358+Ali-Aseman@users.noreply.github.com>
2025-05-05 19:11:21 +0330

    Update server.js

--- private-site/server.js
+++ private-site/server.js
@@ -2,15 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// تنظیم موتور EJS
 app.set("view engine", "ejs");
 
-// روت اصلی
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-// اجرا
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
